48b0af08ad8ebbe199e7ef141c743b758fd7efc0
ekayudinata <ekayudinatha@gmail.com>
2018-12-01 12:45:58 +0800

    ubah layout lelang dan gradel appcompact

--- app/src/main/java/com/dionrasmadi/lelangapp/listLelang/LelangActivity.java
+++ app/src/main/java/com/dionrasmadi/lelangapp/listLelang/LelangActivity.java
@@ -1,11 +1,24 @@
 package com.dionrasmadi.lelangapp.listLelang;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.widget.ProgressBar;
 
 import com.dionrasmadi.lelangapp.R;
+import com.dionrasmadi.lelangapp.dataClass.DataBarangLelang;
+
+import java.util.List;
 
 public class LelangActivity extends AppCompatActivity {
+
+    Intent intent;
+
+    static ProgressBar progressBar;
+    static RecyclerView recyclerView;
+    static List<DataBarangLelang> barang;
+    static LelangAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
