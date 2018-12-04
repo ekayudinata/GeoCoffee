fdc3c5c26952de6e1c9c6784c8575ec0684aa0f0
ekayudinata <ekayudinatha@gmail.com>
2018-12-04 09:03:48 +0800

    tambah activity tambah lelang

--- app/src/main/java/com/dionrasmadi/lelangapp/HomeActivity.java
+++ app/src/main/java/com/dionrasmadi/lelangapp/HomeActivity.java
@@ -1,9 +1,15 @@
 package com.dionrasmadi.lelangapp;
 
+import android.Manifest;
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,8 +21,9 @@ import com.dionrasmadi.lelangapp.listLelang.LelangActivity;
 import com.dionrasmadi.lelangapp.mapPenjual.MapPetaniActivity;
 
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
-import com.google.android.gms.common.GooglePlayServicesRepairableException;
-
+    import com.google.android.gms.common.GooglePlayServicesRepairableException;
+    import com.google.android.gms.location.places.Place;
+    import com.google.android.gms.location.places.ui.PlacePicker;
 public class HomeActivity extends AppCompatActivity {
 
     private final static int MY_PERMISSION_FINE_LOCATION = 101;
@@ -35,6 +42,8 @@ public class HomeActivity extends AppCompatActivity {
         longitude = intent.getStringExtra("longitude");
         username  = intent.getStringExtra("username");
 
+        requestPermission();
+
         dialog = new Dialog(HomeActivity.this);
         dialog.setContentView(R.layout.layout_loading);
         dialog.setCanceledOnTouchOutside(false);
@@ -52,7 +61,6 @@ public class HomeActivity extends AppCompatActivity {
 
     public void toLelangSaya(View view) {
         if ((latitude.equals("")) && (longitude.equals(""))){
-
             new AlertDialog.Builder(this)
                     .setTitle("Informasi")
                     .setMessage("Lokasi Anda Masih Kosong,Masukkan Lokasi anda Sekarang ?")
@@ -60,7 +68,17 @@ public class HomeActivity extends AppCompatActivity {
                     .setPositiveButton("ya", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
-                            Toast.makeText(HomeActivity.this, "masuk ke maps", Toast.LENGTH_SHORT).show();
+
+                            PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+                            try {
+                                Intent intent = builder.build(HomeActivity.this);
+                                startActivityForResult(intent,PLACE_PICKER_REQUEST);
+                            } catch (GooglePlayServicesRepairableException e) {
+                                e.printStackTrace();
+                            } catch (GooglePlayServicesNotAvailableException e) {
+                                e.printStackTrace();
+                            }
+
                         }
                     })
                     .setNegativeButton("tidak",null).show();
@@ -73,4 +91,56 @@ public class HomeActivity extends AppCompatActivity {
         }
     }
 
+    private void requestPermission(){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSION_FINE_LOCATION);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode){
+            case MY_PERMISSION_FINE_LOCATION:
+                if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(getApplicationContext(), "This app requires location permissions to be granted", Toast.LENGTH_LONG).show();
+                    finish();
+                }
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PLACE_PICKER_REQUEST){
+            if (resultCode==RESULT_OK){
+                Place place = PlacePicker.getPlace(HomeActivity.this,data);
+                Double lat = place.getLatLng().latitude;
+                Double longt = place.getLatLng().longitude;
+                latitude = String.valueOf(lat);
+                longitude = String.valueOf(longt);
+
+                Log.d("longt : ", longitude);
+                new AlertDialog.Builder(this)
+                        .setTitle("Lokasi anda :" )
+                        .setMessage("Latitude :"+latitude+
+                                "Longitude :"+longitude+
+                                "Simpan Lokasi ? ")
+                        .setCancelable(false)
+                        .setPositiveButton("ya", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(HomeActivity.this, "panggil background worker", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .setNegativeButton("tidak",null).show();
+
+            }
+        }
+
+    }
 }
--- app/src/main/java/com/dionrasmadi/lelangapp/listLelang/LelangActivity.java
+++ app/src/main/java/com/dionrasmadi/lelangapp/listLelang/LelangActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
@@ -29,12 +30,24 @@ public class LelangActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lelang);
 
+        Toolbar toolbar = findViewById(R.id.toolbar_lelang);
+        setSupportActionBar(toolbar);
+        if(getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
         FloatingActionButton floatingActionButton=(FloatingActionButton) findViewById(R.id.add_lelang);
         floatingActionButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view){
                 Toast.makeText(view.getContext(),"tambah Lelang",Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    @Override
+    public boolean onSupportNavigateUp(){
+        finish();
+        return true;
     }
 
 }
